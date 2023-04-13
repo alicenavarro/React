1b037497507b92d79352d2e5fda31fceb1891e52
alicenavarro <ali.casanova.navarro@gmail.com>
2023-04-13 23:37:04 +0200

    holi

---
+++ my-app/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a My Browser</h1>
+      <p>Este es un navegador web creado con React y React Router.</p>
+    </div>
+  );
+}
+
+export default Home;
